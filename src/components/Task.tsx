f2d2b842a79a94b5e864e00a4362b7aca72326de
Mitsudani <mdsachetti@gmail.com>
2022-07-02 03:59:10 -0300

    Task states added to component. Adon-ally installed

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -2,8 +2,8 @@ import { ReactElement } from "react";
 
 export interface TaskProps {
   task: { id: string; title: string; state: string };
-  onArchiveTask: () => void;
-  onPinTask: () => void;
+  onArchiveTask: (id: string) => void;
+  onPinTask: (id: string) => void;
 }
 
 const Task = ({
@@ -12,10 +12,43 @@ const Task = ({
   onPinTask,
 }: TaskProps): ReactElement => {
   return (
-    <div className="list-item">
-      <label htmlFor="title" aria-label={title}>
-        <input type="text" value={title} readOnly={true} name="title" />
+    <div className={`list-item ${state}`}>
+      <label
+        htmlFor="checked"
+        aria-label={`archiveTask-${id}`}
+        className="checkbox"
+      >
+        <input
+          type="checkbox"
+          disabled={true}
+          name="checked"
+          id={`archiveTask-${id}`}
+          checked={state === "TASK_ARCHIVED"}
+        />
+        <span className="checkbox-custom" onClick={() => onArchiveTask(id)} />
       </label>
+
+      <label htmlFor="title" aria-label={title} className="title">
+        <input
+          type="text"
+          value={title}
+          readOnly={true}
+          name="title"
+          placeholder="Input title"
+        />
+      </label>
+
+      {state !== "TASK_ARCHIVED" && (
+        <button
+          className="pin-button"
+          onClick={() => onPinTask(id)}
+          id={`pinTask-${id}`}
+          aria-label={`pinTask-${id}`}
+          key={`pinTask-${id}`}
+        >
+          <span className={`icon-star`} />
+        </button>
+      )}
     </div>
   );
 };
